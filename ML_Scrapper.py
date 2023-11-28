3f0c6941d1faad0fb5dbabb013674518c8fbd9fa
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-11-28 07:17:43 -0300

    chore: usado pandas para excel, limpado preço, adicionado EdgeOptions
    Substitui openpyxl por pandas para criar Excel devido familiaridade. Limpei "price" no código. Adicionado EdgeOptions para customização de Edge WebDriver.

--- ML_Scrapper.py
+++ ML_Scrapper.py
@@ -1,22 +1,31 @@
-#Importação das bibliotecas necessárias
+# Importação das bibliotecas necessárias
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from msedge.selenium_tools import Edge, EdgeOptions
 from time import sleep
-from openpyxl import Workbook
+import pandas as pd
 
-#Listas utilizadas para percorrer os produtos pesquisados
+# Listas utilizadas para percorrer os produtos pesquisados
 links = []
 produtos = []
 
-#Solicitação de produto ao usuário
+# Solicitação de produto ao usuário
 produto = input('Digite o que deseja pesquisar: ').strip()
 
-#Tratamento de texto para a pesquisa do produto
+# Tratamento de texto para a pesquisa do produto
 produto_tratado = produto.replace(' ', '-')
 
-#Usando o WebDriver para aquisição dos produtos pesquisados
-driver = webdriver.Edge()
+# Usando o WebDriver para aquisição dos produtos pesquisados
+# Tenho problemas usando Selenium no meu PC, então preciso especificar o caminho.
+path = r"C:/Users/deimo/Desktop/Global_Weather/msedgedriver.exe"
+#EdgeOptions é para ficar mais fácil customizar, aqui estou usando Chromium e abrindo o navegador sem ele aparecer, tem como abrir em privado também, é para customizar apenas
+options = EdgeOptions()
+options.use_chromium = True
+options.add_argument("--headless")
+driver = Edge(executable_path=path, options=options)
+
 driver.get(f'https://lista.mercadolivre.com.br/{produto_tratado}')
+
 sleep(2)
 elementos = driver.find_elements(By.XPATH, '//*/div[2]/div[1]/div/a')
 for elemento in elementos:
@@ -30,20 +39,19 @@ for link in links:
     sleep(1)
     titulo = driver.find_element(By.TAG_NAME, 'h1').text
     preco = driver.find_element(By.CSS_SELECTOR, 'div.ui-pdp-price__second-line > span > span:nth-child(1)').text
-    if preco.__contains__('com'):
-        preco = preco.replace(' reais com ', ',')
-        preco = preco.replace(' centavos', '')
-    else:  
-        preco = preco.replace(' reais', ',00')
+    # \n bugando a visualização no excel, tentando eliminar o problema
+    preco = preco.replace('\n', '')
+    # Valor vindo repetido "749,55R$749,55" usando o if preco "#if preco.__contains__('com'):"
+    preco = "R$" + preco.split("R$")[1].strip()  # Mantendo apenas o valor depois do primeiro "R$"
+    #if preco.__contains__('com'):
+    #    preco = preco.replace(' reais com ', ',')
+    #    preco = preco.replace(' centavos', '')
+    #else:  
+    #    preco = preco.replace(' reais', ',00')
+
     produtos.append({'título': titulo, 'preço': preco})
 
-#Usando Openpyxl para criar o arquivo xlsx(Excel)
-arquivo = Workbook()
-pagina = arquivo.active
-pagina.title = f'Mercado Livre - {produto} results'
-pagina['A1'] = 'Produto'
-pagina['B1'] = 'Preço'
-for indice, produto in enumerate(produtos):
-    pagina[f'A{indice + 2}'] = produto['título']
-    pagina[f'B{indice + 2}'] = produto['preço']
-arquivo.save('Mercado Livre Scrapper.xlsx')
+# Tive problemas ao visualizar a planilha criada pelo openpyxl, valores repetidos e prompt to excel tentando recuperar alguns valores
+# Usando pandas para criar o arquivo xlsx(Excel)
+df = pd.DataFrame(produtos)
+df.to_excel('Mercado Livre Scrapper.xlsx', index=False)
